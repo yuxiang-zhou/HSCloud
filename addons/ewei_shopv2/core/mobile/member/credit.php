534802b22cc6ce93be91d4c368de0f3db75baab9
Yuxiang Zhou <mr.yuxiang.zhou@googlemail.com>
2017-01-23 10:40:13 +0000

    added mobile view

---
+++ addons/ewei_shopv2/core/mobile/member/credit.php
@@ -0,0 +1,21 @@
+<?php
+//加密方式：Ioncube 7.X/8.X/9.X加密，代码还原率98+%以上(变量数值会有问题需手动修复)。
+//VIP会员:zh6691 您好,破解:ioncube加密,本次扣金币:15个,金币余额:100个,感谢您的支持.
+//此程序由【找源码】http://Www.ZhaoYuanMa.Com (VIP会员功能）在线逆向还原，QQ：7530782
+?>
+<?php
+if (!(defined('IN_IA')))
+{
+	exit('Access Denied');
+}
+class Credit_EweiShopV2Page extends MobilePage
+{
+	public function main()
+	{
+		global $_W;
+		global $_GPC;
+
+		include $this->template();
+	}
+}
+?>
